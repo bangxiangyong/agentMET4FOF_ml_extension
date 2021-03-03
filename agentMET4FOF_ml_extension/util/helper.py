f7d094d021b487a0997e299f25797521261c1121
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-03-03 13:08:15 +0000

    compute mean and std helper function

--- agentMET4FOF_ml_extension/util/helper.py
+++ agentMET4FOF_ml_extension/util/helper.py
@@ -5,3 +5,10 @@ def clip01(x_test, min=0, max=100):
 
 def move_axis(x, first_axis=1,second_axis=2):
     return np.moveaxis(x,first_axis,second_axis)
+
+def compute_mean(x, axis=0):
+    return np.mean(x,axis=axis)
+
+def compute_mean_std(x, axis=0):
+    data = np.array([np.mean(x,axis=axis), np.std(x,axis=axis)])
+    return data
