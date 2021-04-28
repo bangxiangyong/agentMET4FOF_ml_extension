26bd7df205d8054e8692e9eeca71064eaf7335ee
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-04-28 09:37:54 +0100

    feat(ml_agents) : implement evaluate auroc agent

--- agentMET4FOF_ml_extension/evaluate_agents.py
+++ agentMET4FOF_ml_extension/evaluate_agents.py
@@ -16,7 +16,7 @@ import functools
 import pandas as pd
 # Datastream Agent
 from .ml_agents import ML_BaseAgent
-from .util.calc_auroc import calc_all_scores
+from .util.calc_auroc import calc_all_scores, calc_auroc_score
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
@@ -101,8 +101,48 @@ class EvaluateSupervisedUncAgent(ML_BaseAgent):
 
 
 
+class EvaluateAUROCAgent(ML_BaseAgent):
+    """
+    Takes in BAE mean-var NLL of test inlier and ood and squashes them to 1 dimension as the anomaly score.
+    Calculates the AUROC for the inlier and ood scores.
+
+    Should be used in tandem with `compute_mean_var` agent with return_dict =True
+    """
+    def init_parameters(self, figsize=(10,5)):
+        self.figsize = figsize
+
+    def on_received_message(self, message):
+        channel = message["channel"]
+
+        if channel == "test":
+            msg_data = message["data"]
+            x_test = msg_data["quantities"]["test"]
+            x_ood = msg_data["quantities"]["ood"]
+            plots = []
+            for key in list(x_test.keys()):
+                x_test_, x_ood_ = self.sum_mean_var(x_test[key],x_ood[key])
+                auroc = calc_auroc_score(x_test_, x_ood_)
+
+                plots.append(self.plot_histogram(x_test_,x_ood_, title="AUROC-"+key+":"+str(round(auroc,2))))
+                self.send_output({"AUROC-"+key: auroc}, channel=channel)
+            self.send_plot(plots)
 
 
+    def sum_mean_var_(self, bae_test):
+        while len(bae_test.shape)>1:
+            bae_test = bae_test.sum(-1)
+        return bae_test
+
+    def sum_mean_var(self, *bae_test):
+        return (self.sum_mean_var_(bae_test_i) for bae_test_i in bae_test)
+
+    def plot_histogram(self, x_test,x_ood, title=""):
+        fig = plt.figure()
+        plt.hist(x_test)
+        plt.hist(x_ood)
+        plt.legend(["TEST","OOD"])
+        plt.title(title)
+        return fig
 
 
 
