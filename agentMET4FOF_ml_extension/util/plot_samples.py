a43906554a64bb261dbf420b80a5f048b4f855ae
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-04-28 09:29:01 +0100

    feat(resample): helper to plot time series with metadata

--- agentMET4FOF_ml_extension/util/plot_samples.py
+++ agentMET4FOF_ml_extension/util/plot_samples.py
@@ -3,19 +3,22 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
 
-def plot_time_series_samples(x, y_target, metadata,  sensor_axis=1, figsize=(18,5), nrows=3, ncols=5):
+def plot_time_series_samples(x, y_target, metadata=None,  sensor_axis=1, figsize=(18,5), nrows=3, ncols=5):
     plots = []
     if isinstance(x, dict):
         for key in x.keys():
-            plots.append(plot_time_series_samples_(x[key], y_target[key], metadata,
+            plots.append(plot_time_series_samples_(x[key], y_target[key], metadata=metadata,
                                                    sensor_axis=sensor_axis,figsize=figsize,
                                                    nrows=nrows,ncols=ncols, suptitle="Axis: "+key))
     return plots
 
-def plot_time_series_samples_(ts_x, y_target, metadata,  sensor_axis=1, figsize=(18,5), nrows=3, ncols=5, suptitle=""):
-    units = metadata["units"]
-    labels = metadata["labels"]
-
+def plot_time_series_samples_(ts_x, y_target, metadata=None,  sensor_axis=1, figsize=(18,5), nrows=3, ncols=5, suptitle=""):
+    if metadata is not None:
+        units = metadata["units"]
+        labels = metadata["labels"]
+        metadata_able = True
+    else:
+        metadata_able = False
     num_sensors = ts_x.shape[sensor_axis]
     num_examples = ts_x.shape[0]
     num_samples = ts_x.shape[1 if sensor_axis ==2 else 2]
@@ -30,8 +33,9 @@ def plot_time_series_samples_(ts_x, y_target, metadata,  sensor_axis=1, figsize=
                 ts_sample = ts_x[np.random.randint(num_examples), :, sensor_i]
 
             axes[sensor_i].plot(np.arange(num_samples),ts_sample)
-            axes[sensor_i].set_ylabel(units[sensor_i])
-            axes[sensor_i].set_title(labels[sensor_i])
+            if metadata_able:
+                axes[sensor_i].set_ylabel(units[sensor_i])
+                axes[sensor_i].set_title(labels[sensor_i])
 
     # remove unsused sensors
     if num_axes > num_sensors:
